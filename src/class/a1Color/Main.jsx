674d9b383c9999bc34b881b23b3c7836463e71f5
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2024-03-25 16:08:44 -0300

    make first class look better

--- src/class/a1Color/Main.jsx
+++ src/class/a1Color/Main.jsx
@@ -1,38 +1,40 @@
 import red from './color/red.png'
 import blue from './color/blue.png'
 import yellow from './color/yellow.png'
+import green from './color/green.png'
 import purple from './color/Purple.png'
 import black from './color/black.jpg'
 import white from './color/white.png'
 import brown from './color/brown.png'
 import orange from './color/orange.jpeg'
 import pink from './color/pink.png'
-import green from './color/green.png'
 import cyan from './color/cyan.png'
+import gray from './color/gray.png'
 
-function ColorList({colorText, colorImg}){
-return (<li className='w-full flex items-center justify-center text-center'>
-<figure className='w-full flex-col-reverse  flex items-center justify-center text-center'>
-<img className='w-11/12 border-black border-2 rounded-3xl grid-cols-auto' src={colorImg} alt={colorText}/>
-<figcaption>{colorText}</figcaption>
-</figure>
-</li>
-)
+function ColorList({ colorText, colorImg }) {
+	return (<li className='w-full flex justify-center  text-center'>
+		<figure className='w-full flex-col-reverse flex text-center'>
+			< img className='w-full h-5/6 border-black border-2 rounded-3xl ' src={colorImg} alt={colorText} />
+			<figcaption>{colorText}</figcaption>
+		</figure >
+	</li >
+	)
 }
 
-export  let A1Color = (<ul className='flex flex-col '>
-	<ColorList colorText='red' colorImg={red} / >
-	<ColorList colorText='blue' colorImg={blue} / >
-	<ColorList colorText='green' colorImg={green} / >
-	<ColorList colorText='black' colorImg={black} / >
-	<ColorList colorText='white' colorImg={white} / >
-	<ColorList colorText='yellow' colorImg={yellow} / >
-	<ColorList colorText='orange' colorImg={orange} / >
-	<ColorList colorText='pink' colorImg={pink} / >
-	<ColorList colorText='purple' colorImg={purple} / >
-	<ColorList colorText='brown' colorImg={brown} / >
-	<ColorList colorText='cyan' colorImg={cyan} / >
-	</ul>
+export let A1Color = (<ul className='grid grid-cols-autoFit-2 gap-2  justify-items-center'>
+	<ColorList colorText='Red' colorImg={red} />
+	< ColorList colorText='Blue' colorImg={blue} />
+	<ColorList colorText='Green' colorImg={green} />
+	<ColorList colorText='Black' colorImg={black} />
+	<ColorList colorText='White' colorImg={white} />
+	<ColorList colorText='Yellow' colorImg={yellow} />
+	<ColorList colorText='Orange' colorImg={orange} />
+	<ColorList colorText='Pink' colorImg={pink} />
+	<ColorList colorText='Purple' colorImg={purple} />
+	<ColorList colorText='Brown' colorImg={brown} />
+	<ColorList colorText='Cyan' colorImg={cyan} />
+	<ColorList colorText='Gray' colorImg={gray} />
+</ul >
 )
 // export default function Main(){
 // return(
