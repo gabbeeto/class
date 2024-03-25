c0b07fa7e91fcb4fd70d3ba5c243ea13de13a280
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2024-03-25 16:26:23 -0300

    hide colors for class

--- src/class/a1Color/Main.jsx
+++ src/class/a1Color/Main.jsx
@@ -10,32 +10,39 @@ import orange from './color/orange.jpeg'
 import pink from './color/pink.png'
 import cyan from './color/cyan.png'
 import gray from './color/gray.png'
+import { useState } from 'react'
 
-function ColorList({ colorText, colorImg }) {
+function ColorList({ colorText, colorImg, isHidden }) {
 	return (<li className='w-full flex justify-center  text-center'>
 		<figure className='w-full flex-col-reverse flex text-center'>
 			< img className='w-full h-5/6 border-black border-2 rounded-3xl ' src={colorImg} alt={colorText} />
-			<figcaption>{colorText}</figcaption>
+			<figcaption className={isHidden && 'hidden'}>{colorText}</figcaption>
 		</figure >
 	</li >
 	)
 }
 
-export let A1Color = (<ul className='grid grid-cols-autoFit-2 gap-2  justify-items-center'>
-	<ColorList colorText='Red' colorImg={red} />
-	< ColorList colorText='Blue' colorImg={blue} />
-	<ColorList colorText='Green' colorImg={green} />
-	<ColorList colorText='Black' colorImg={black} />
-	<ColorList colorText='White' colorImg={white} />
-	<ColorList colorText='Yellow' colorImg={yellow} />
-	<ColorList colorText='Orange' colorImg={orange} />
-	<ColorList colorText='Pink' colorImg={pink} />
-	<ColorList colorText='Purple' colorImg={purple} />
-	<ColorList colorText='Brown' colorImg={brown} />
-	<ColorList colorText='Cyan' colorImg={cyan} />
-	<ColorList colorText='Gray' colorImg={gray} />
-</ul >
-)
+export function A1Color() {
+	const [hid, setHid] = useState(false)
+	return (<>
+		<ul className='grid grid-cols-autoFit-2 gap-2  justify-items-center'>
+			<ColorList colorText='Red' colorImg={red} isHidden={hid} />
+			<ColorList colorText='Blue' colorImg={blue} isHidden={hid} />
+			<ColorList colorText='Green' colorImg={green} isHidden={hid} />
+			<ColorList colorText='Black' colorImg={black} isHidden={hid} />
+			<ColorList colorText='White' colorImg={white} isHidden={hid} />
+			<ColorList colorText='Yellow' colorImg={yellow} isHidden={hid} />
+			<ColorList colorText='Orange' colorImg={orange} isHidden={hid} />
+			<ColorList colorText='Pink' colorImg={pink} isHidden={hid} />
+			<ColorList colorText='Purple' colorImg={purple} isHidden={hid} />
+			<ColorList colorText='Brown' colorImg={brown} isHidden={hid} />
+			<ColorList colorText='Cyan' colorImg={cyan} isHidden={hid} />
+			<ColorList colorText='Gray' colorImg={gray} isHidden={hid} />
+		</ul >
+		<button onClick={() => setHid(!hid)} className='border-2 border-black p-1 mt-2 rounded-2xl text-center bg-cyan-200 text-black'> hide colors</button>
+	</>
+	)
+}
 // export default function Main(){
 // return(
 // 	<ul>
--- src/hashAndContent.jsx
+++ src/hashAndContent.jsx
@@ -1,15 +1,16 @@
-import {A1Color} from './class/a1Color/Main'
+import { A1Color } from './class/a1Color/Main'
 
 const hashContent = {
-	"#a1color": ['color', A1Color]
+	"#a1color": ['color', <A1Color />]
 }
 
 
-export default function ReturnContent(){
-const hash = window.location.hash 
-if(hash == '' || !(hash in (hashContent))){
-return ['invalid classroom', (<h3>selecciona un link correcto</h3>) ]}
-else{
-return hashContent[hash]
-}
+export default function ReturnContent() {
+	const hash = window.location.hash
+	if (hash == '' || !(hash in (hashContent))) {
+		return ['invalid classroom', (<h3>selecciona un link correcto</h3>)]
+	}
+	else {
+		return hashContent[hash]
+	}
 }
