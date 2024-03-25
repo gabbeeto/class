9e2dd6e23a9e7c1f9c9a27630509651851082b87
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2024-03-25 08:12:12 -0300

    make a structure

---
+++ src/master.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+let rootElement = document.getElementById('root')
+let root = ReactDOM.createRoot(rootElement)
+
+root.render(
+	<h1>hello</h1>
+)
