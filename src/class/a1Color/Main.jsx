53353f03fbc5c0580ae188556118f9a08e0d158f
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2024-03-26 09:47:49 -0300

    make separation of concern

--- src/class/a1Color/Main.jsx
+++ src/class/a1Color/Main.jsx
@@ -10,105 +10,25 @@ import orange from './color/orange.jpeg'
 import pink from './color/pink.png'
 import cyan from './color/cyan.png'
 import gray from './color/gray.png'
-import ReactDOM from 'react-dom/client'
+import {ListOfWords} from './../vocabList'
 
-let colorRoot
-let textHidden = false
 const colors = [
-		<ColorList colorText='Red' colorImg={red}  />,
-		<ColorList colorText='Blue' colorImg={blue}  />,
-		<ColorList colorText='Green' colorImg={green}  />,
-		<ColorList colorText='Black' colorImg={black}  />,
-		<ColorList colorText='White' colorImg={white}  />,
-		<ColorList colorText='Yellow' colorImg={yellow}  />,
-		<ColorList colorText='Orange' colorImg={orange}  />,
-		<ColorList colorText='Pink' colorImg={pink}  />,
-		<ColorList colorText='Purple' colorImg={purple}  />,
-		<ColorList colorText='Brown' colorImg={brown}  />,
-		<ColorList colorText='Cyan' colorImg={cyan}  />,
-		<ColorList colorText='Gray' colorImg={gray}  />
+		['Red', [red]],
+		['Blue', [blue]],
+		['Green', [green]],
+		['Black', [black]],
+		['White', [white]],
+		['Yellow', [yellow]],
+		['Orange', [orange]],
+		['Pink', [pink]],
+		['Purple', [purple]],
+		['Brown', [brown]],
+		['Cyan', [cyan]],
+		['Gray', [gray]],
 	]
 
 
-
-function ColorList({ colorText, colorImg }) {
-	return (<li className='w-full flex justify-center  text-center'>
-		<figure className='w-full flex-col-reverse flex text-center'>
-			< img className='w-full h-5/6 border-black border-2 rounded-3xl ' src={colorImg} alt={colorText} />
-			<figcaption >{colorText}</figcaption>
-		</figure >
-	</li >
-	)
+export function A1Color(){
+return (<ListOfWords list={colors} name={'color'} />
+)
 }
-
-export function A1Color() {
-
-
-	return (<>
-		<ul id='listOfColors' className='grid grid-cols-autoFit-2 gap-2  justify-items-center'>
-			{randomize(colors)}
-		</ul >
-		<div className='flex flex-rows gap-2 items-center justify-center'>
-			<button  id="shower" onClick={hideOrUnHide} className='border-2 border-black p-1 mt-2 rounded-2xl text-center bg-cyan-200 text-black'>show colors</button>
-
-			<button onClick={regenerateColors} className='border-2 border-black p-1 mt-2 rounded-2xl text-center bg-cyan-200 text-black'>change order</button>
-		</div>
-	</>
-	)
-}
-
-
-function regenerateColors(){
-const theColors = [
-		<ColorList colorText='Red' colorImg={red}  />,
-		<ColorList colorText='Blue' colorImg={blue}  />,
-		<ColorList colorText='Green' colorImg={green}  />,
-		<ColorList colorText='Black' colorImg={black}  />,
-		<ColorList colorText='White' colorImg={white}  />,
-		<ColorList colorText='Yellow' colorImg={yellow}  />,
-		<ColorList colorText='Orange' colorImg={orange}  />,
-		<ColorList colorText='Pink' colorImg={pink}  />,
-		<ColorList colorText='Purple' colorImg={purple}  />,
-		<ColorList colorText='Brown' colorImg={brown}  />,
-		<ColorList colorText='Cyan' colorImg={cyan}  />,
-		<ColorList colorText='Gray' colorImg={gray}  />
-	]
-
-let listOfColors = document.querySelector('#listOfColors')
-if(typeof colorRoot != 'undefined'){
-colorRoot.render(<>{randomize(theColors)}</>)
-}
-else{
-colorRoot = ReactDOM.createRoot(listOfColors)
-colorRoot.render(<>{randomize(theColors)}</>)
-}
-}
-
-function hideOrUnHide(){
-textHidden = !textHidden;
-let figCaps = document.querySelectorAll('figcaption')
-let shower = document.querySelector('shower')
-if(textHidden){
-figCaps.forEach(figCap => figCap.classList.add('hidden'))
-shower.innerHTML = 'show colors'
-}
-else{
-figCaps.forEach(figCap => figCap.classList.remove('hidden'))
-shower.innerHTML = 'hide colors'
-}
-}
-
-// improve this
-function randomize(arrayToRandomize) {
-	const lengthForRandomnes = arrayToRandomize.length
-	let newArray = arrayToRandomize
-	let newArrayWithAnotherOrder = [];
-	for (let index = lengthForRandomnes; index > 0; index--) {
-		let randomNumber = Math.floor(Math.random() * newArray.length)
-
-		newArrayWithAnotherOrder.push(newArray[randomNumber])
-		newArray.splice(randomNumber, 1)
-	}
-	return newArrayWithAnotherOrder;
-}
-
---
+++ src/class/vocabList.jsx
@@ -0,0 +1,78 @@
+import ReactDOM from 'react-dom/client'
+
+let wordRoot
+let textHidden = false
+let newList
+
+
+function WordList({ wordText, wordImg }) {
+	let randomNumber = Math.floor(Math.random() * wordImg.length)
+	let randomImg = wordImg[randomNumber]
+	return (<li className='w-full flex justify-center  text-center'>
+		<figure className='w-full flex-col-reverse flex text-center'>
+			< img className='w-full h-5/6 border-black border-2 rounded-3xl ' src={randomImg} alt={wordText} />
+			<figcaption >{wordText}</figcaption>
+		</figure >
+	</li >
+	)
+}
+
+export function ListOfWords({list, name}) {
+
+   newList = list.map((word) => {return <WordList wordText={word[0]} wordImg={word[1]}  /> })
+randomize()
+
+	return (<>
+		<ul id='listOfWords' className='grid grid-cols-autoFit-2 gap-2  justify-items-center'>
+			{newList}
+		</ul >
+		<div className='flex flex-rows gap-2 items-center justify-center'>
+			<button  id="shower" onClick={() => hideOrUnHide(name)} className='border-2 border-black p-1 mt-2 rounded-2xl text-center bg-cyan-200 text-black'>hide {name}s</button>
+
+			<button onClick={regenerateWords} className='border-2 border-black p-1 mt-2 rounded-2xl text-center bg-cyan-200 text-black'>change order</button>
+		</div>
+	</>
+	)
+}
+
+
+function regenerateWords(){
+randomize(newList)
+let listOfWords = document.querySelector('#listOfWords')
+if(typeof wordRoot != 'undefined'){
+wordRoot.render(<>{newList}</>)
+}
+else{
+wordRoot = ReactDOM.createRoot(listOfWords)
+wordRoot.render(<>{newList}</>)
+}
+}
+
+function hideOrUnHide(name){
+textHidden = !textHidden;
+let figCaps = document.querySelectorAll('figcaption')
+let shower = document.querySelector('#shower')
+if(textHidden){
+figCaps.forEach(figCap => figCap.classList.add('hidden'))
+shower.innerHTML = `show ${name}s`
+}
+else{
+figCaps.forEach(figCap => figCap.classList.remove('hidden'))
+shower.innerHTML = `hide ${name}s`
+}
+}
+
+function randomize() {
+	let arrayToRandomize = [...newList]
+	const lengthForRandomnes = arrayToRandomize.length
+	let newArray = arrayToRandomize
+	let newArrayWithAnotherOrder = [];
+	for (let index = lengthForRandomnes; index > 0; index--) {
+		let randomNumber = Math.floor(Math.random() * newArray.length)
+
+		newArrayWithAnotherOrder.push(newArray[randomNumber])
+		newArray.splice(randomNumber, 1)
+	}
+	newList = [...newArrayWithAnotherOrder];
+}
+
