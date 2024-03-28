82c4dc7cfa1f119d30ce6de079d8ccd8def7645d
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2024-03-28 15:34:35 -0300

    add explanation for spanish

--- src/class/a1_1/Main.jsx
+++ src/class/a1_1/Main.jsx
@@ -9,10 +9,24 @@ return (<>
 	<TinyExplanation title={['adjetive', 'adjetivo']} explanation={['what describes the noun','lo que describe el sustantivo']} color='orange'  example="blue" / >
 
 
-	< ExplanationContainer explanation="En Inglés, los adjetivos(lo que describen a la cosa) van hacia a la isquierda de los sustantivos(la cosa)" color="green"  example={<>
-<span className='bg-amber-700'>blue</span>  <span className='bg-indigo-700 '>cat</span>
-		</>} / >
+	< ExplanationContainer explanation="En Inglés, los adjetivos(lo que describen a la cosa) siempre van hacia a la isquierda de los sustantivos(la cosa)" color="green"  example={<>
+<span className='bg-amber-700'>blue</span>  <span className='bg-indigo-700 '>cat</span></>} />
 
+
+
+
+	< ExplanationContainer explanation="En Español, los adjetivos(lo que describen a la cosa) aveces van hacia a la derecha de los sustantivos(la cosa)" color="green"  example={(<div className='flex flex-col'>
+
+		<div>
+<span className='bg-indigo-700 '>gato</span>  <span className='bg-amber-700'>azul</span>  
+</div>
+
+		<div>
+<span className='bg-amber-700'>buen</span>  <span className='bg-indigo-700 '>perro</span>     
+</div>
+
+	</div>
+)} />
 </>
 )
 }
