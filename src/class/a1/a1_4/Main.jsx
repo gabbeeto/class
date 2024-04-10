688c68e1e18f538244676ba7e701eec513e908c0
¨gabbeeto¨ <124828006+gabbeeto@users.noreply.github.com>
2024-04-10 11:01:11 -0300

    make the tests for the forth class

--- src/class/a1/a1_4/Main.jsx
+++ src/class/a1/a1_4/Main.jsx
@@ -1,4 +1,4 @@
-import {Word, TinyExplanation, ExplanationContainer, TitleContainer} from './../../explanationUtilities'
+import {Word, TinyExplanation, ExplanationContainer, TitleContainer, ListOfTest} from './../../explanationUtilities'
 
 
 export function A1_4(){
@@ -39,6 +39,32 @@ return (<>
 		<div className='flex flex-col bg-sky-700 rounded-2xl p-2'><span>You hate Minecraft</span><span>he hate<span className='text-emerald-200'>s</span> Minecraft</span></div>
 </>)} / >
 
+	<Word word={[(<>the</>), (<>El/La/Los/Las</>)]}/>
+	<Word word={[(<>the cat</>), (<>el gato</>)]}/>
+	<Word word={[(<>the cow</>), (<>la vaca</>)]}/>
+	<Word word={[(<>the dogs</>), (<>los perros</>)]}/>
+	<Word word={[(<>the dogs</>), (<>las perras</>)]}/>
+	<Word word={[(<>the fish</>), (<>él pez</>)]}/>
+	<Word word={[(<>the fish</>), (<>los peces</>)]}/>
+
+	<ListOfTest list={
+		[
+		[(<>ella come pizza</>), (<>she eats pizza</>)],
+		[(<>el gato toma agua</>), (<>the cat drinks water</>)],
+		[(<>los perros toman leche</>), (<>the dogs drink milk</>)],
+		[(<>como manzana</>), (<>I eat apple</>)],
+		[(<>él come banana</>), (<>he eats banana</>)],
+		[(<>comes banana</>), (<>you eat banana</>)],
+		[(<>el caballo y Maria comen carne</>), (<>the horse and Maria eat meat</>)],
+		[(<>los peces toman té</>), (<>the fish drink tea</>)],
+		[(<>él pez toma té</>), (<>the fish drinks tea</>)],
+		[(<>ella tiene 50 autos</>), (<>she has 50 cars</>)],
+		[(<>amo los aviones</>), (<>I love airplanes</>)],
+		[(<>ella ama las motos</>), (<>she loves motorbikes</>)],
+		[(<>odias Minecraft</>), (<>I hate Minecraft</>)],
+		[(<>él odia My Little Pony</>), (<>he hates My Little Pony</>)],
+		]
+	} / >
 </>);
 
 	
